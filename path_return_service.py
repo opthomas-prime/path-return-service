aa122b3104623dac9e2eb90b30eaf0718fd85776
Thomas Maier <contact@thomas-maier.net>
2019-09-26 11:47:00 +0200

    print SERVICE_NAME env variable in response

--- path_return_service.py
+++ path_return_service.py
@@ -1,7 +1,12 @@
+import os
+
 from bottle import route, run, request
 
 @route('/:#.*#')
 def index():
-    return request.fullpath
+    if 'SERVICE_NAME' in os.environ:
+        service_name = os.environ['SERVICE_NAME']
+        return '%s: %s' % (service_name, request.fullpath)
+    return '%s' % request.fullpath
 
 run(host='0.0.0.0', port=1234)
