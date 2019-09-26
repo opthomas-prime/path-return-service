8b13ee912c057401a11ffe933b20035735a8b7e3
Thomas Maier <contact@thomas-maier.net>
2019-09-26 14:26:39 +0200

    print params aswell

--- path_return_service.py
+++ path_return_service.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from bottle import route, run, request
 
@@ -6,7 +7,7 @@ from bottle import route, run, request
 def index():
     if 'SERVICE_NAME' in os.environ:
         service_name = os.environ['SERVICE_NAME']
-        return '%s: %s' % (service_name, request.fullpath)
-    return '%s' % request.fullpath
+        return '%s: %s\n%s' % (service_name, request.fullpath, json.dumps(request.params.dict))
+    return '%s\n%s' % (request.fullpath, json.dumps(request.params.dict))
 
 run(host='0.0.0.0', port=1234)
